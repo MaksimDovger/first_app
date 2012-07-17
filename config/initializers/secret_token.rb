801a57f7dcfddfad37767945008fae57b9d52702
«Your <everest_moon@mail.ru>
2012-07-17 23:18:37 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd9b4cf1b9f706f7b45207dc786f6e0623bd6ade765750a68594e4f8712db56a567856878a6d97d57dff2e72eba42417b391468e3dac04a20b03255f1e3fb92aa'
